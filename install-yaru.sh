602cf514fd433906a18c2b046184a6e709d01023
wrldmap <123031534+wrldmap@users.noreply.github.com>
2025-08-12 07:38:04 -0500

    Update install-yaru.sh
    oops

--- install-yaru.sh
+++ install-yaru.sh
@@ -4,7 +4,7 @@ sudo pacman -Syu --noconfirm
 sudo pacman -S --needed --noconfirm base-devel git
 mkdir ~/archutils-yaru
 git clone https://aur.archlinux.org/yaru.git ~/archutils-yaru
-cd ~/archutils-yay
+cd ~/archutils-yaru
 makepkg -si --noconfirm --skippgpcheck --noprogressbar --needed
 cd ~
 rm -rf ~/archutils-yaru
