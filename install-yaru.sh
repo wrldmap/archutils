0b4c8cf0cd2dbe796a324ce287847f813bd73b26
wrldmap <123031534+wrldmap@users.noreply.github.com>
2025-08-12 07:10:14 -0500

    Create install-yaru.sh
    literally just the yay script but i replaced a few words

---
+++ install-yaru.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo pacman -Syu --noconfirm
+sudo pacman -S --needed --noconfirm base-devel git
+mkdir ~/archutils-yaru
+git clone https://aur.archlinux.org/yaru.git ~/archutils-yaru
+cd ~/archutils-yay
+makepkg -si --noconfirm --skippgpcheck --noprogressbar --needed
+cd ~
+rm -rf ~/archutils-yaru
+echo "yaru has been installed!"
+yaru --version
